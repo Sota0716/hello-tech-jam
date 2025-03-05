d1212eb2c1ff0b9312633ea912f4231a8a06160b
souta_simono <souta.ss.simono@icloud.com>
2025-03-05 15:15:50 +0900

    検索フォームの位置を変更

---
+++ tech-jam/app/shadcn/page.tsx
@@ -0,0 +1,13 @@
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+export default function Page() {
+  return (
+    <div className="flex min-h-screen items-start justify-center pt-36">
+      <form className="flex items-center space-x-4">
+        <Input type="search" placeholder="検索..." />
+        <Button type="submit">検索</Button>
+      </form>
+    </div>
+  );
+}
